e03523463e2991b6b9cde5f71bf2fb48cd511f6e
haim arka <87427194+haimarka@users.noreply.github.com>
2023-10-02 13:13:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// smooth slide
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e){
+        e.preventDefault(); 
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+// smooth elements apperens
+
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true
+});
+
+sr.reveal(`.about-me, .footer_container`, {
+    origin: 'top',
+    interval: 150
+})
+
+sr.reveal(`.container-left, .projects`, {
+    origin: 'left'
+})
+
+sr.reveal(`.image, .contact`, {
+    origin: 'right'
+})
+
+
+// go back button
+const btn = document.getElementById("goBackBtn");
+if(btn){
+    btn.addEventListener("click", goBack);
+}
+function goBack() {
+    history.back();
+}
